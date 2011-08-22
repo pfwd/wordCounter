de16a68cfa9e36847eca3c94f8a93c848843ba5b
Peter Fisher <work@peterfisher.me.uk>
2011-08-22 19:58:32 +0100

    Formatting code

--- jQuery/wordCounter.jQuery.js
+++ jQuery/wordCounter.jQuery.js
@@ -16,25 +16,31 @@
      * @param array options - Optional
      * @return void
      */
-    $.fn.wordCounter = function(textElement, counterElement, options){
-	var settings = jQuery.extend({
+    $.fn.wordCounter = function(textElement, counterElement, options)
+    {
+	var settings = jQuery.extend(
+	{
 	    limit			: null,
 	    defaultNoLimitFormat	: "Word count: count%",
 	    limitExceededFormat		: "Remove count% word(s)",
 	    limitNotExceededFormat	: "Words left: count% ",
 	    defaultSearch		: "count%",
 	    showNegativeNumbers		: 0
-	}, options);
+	}, 
+	options);
 
 	// Run execute when the window loads
-	$(window).load(function () {
+	$(window).load(function () 
+	{
 	    execute();
 	});
 
 	// Run execute each time the keys have been pressed
-	$('#'+textElement).keyup(function () {
+	$('#'+textElement).keyup(function () 
+	{
 	    execute();
 	});
+	
 	/**
 	 * Executes the logic for the plugin
 	 * Generates the counter
@@ -46,14 +52,18 @@
 	 *
 	 * @return void
 	 */
-	function execute(){
+	function execute()
+	{
 	    var currentCount = countWords();
 	    var outPutCount = 0;
 	    var outPutFormat = 'Problem with word counter';
 	    // Check if  this.limit param is supplied
-	    if(settings.limit != null){
+	    if(settings.limit != null)
+	    {
 		outPutCounter = settings.limit - currentCount;
-	    }else{
+	    }
+	    else
+	    {
 		// Set to default incrmental counter if limit is null
 		outPutCounter = currentCount;
 	    }
@@ -61,38 +71,48 @@
 	    // Check if outPutCounter is greater than 0 (and negative numbers)
 	    // If it is the user has exceeded the limit
 	    // Generate the outPutFormat
-	    if(outPutCounter < 0 && settings.limit != null){
+	    if(outPutCounter < 0 && settings.limit != null)
+	    {
 		// change the negative number to a positve number and set to outPutCounter
-		if(settings.showNegativeNumbers == 0){
+		if(settings.showNegativeNumbers == 0)
+		{
 		    outPutCounter = outPutCounter * -1;
 		}
 		outPutFormat = formatOutput(settings.limitExceededFormat, outPutCounter);
-	    }else if(outPutCounter >= 0 && settings.limit != null){
+	    }
+	    else if(outPutCounter >= 0 && settings.limit != null)
+	    {
 		outPutFormat = formatOutput(settings.limitNotExceededFormat, outPutCounter);
-	    }else if(settings.limit == null){
+	    }
+	    else if(settings.limit == null)
+	    {
 		outPutFormat = formatOutput(settings.defaultNoLimitFormat, outPutCounter);
 	    }
 	    // Update the updateElement using Jquery
 	    $('#'+counterElement).html(outPutFormat);
 	}
+	
 	/**
 	 * Counts the words in the textElement
 	 *
 	 * @return integer
 	 */
-	function countWords(){
+	function countWords()
+	{
 	    var counter = 0;
 	    // Replace the spaces at the start of the string /^\s* and all the spaces at the end \s*$
 	    // while keeping those in-between.
 	    counter = $('#'+textElement).val().replace(/^\s*|\s*$/g,'');
 	    return counter.split(/\s+/).length;
 	}
+	
 	/**
 	 * Formats and returns output for the counter
 	 *
 	 * @return string
 	 */
-	function formatOutput(format, counter){
+	function formatOutput(format, counter)
+	{
 	    return format.replace(settings.defaultSearch, counter);
 	}
     }
